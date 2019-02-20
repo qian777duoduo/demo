4145d15b523d01bc86bf9f59c9ced0c313e5a4ce
v-qianzy <v-qianzy@vanke.com>
2019-02-20 11:35:34 +0800

    密码加密

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,9 +2,14 @@ package com.example.demo.controller;
 
 import com.example.demo.bean.Employee;
 import com.example.demo.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author qianduoduo
@@ -13,15 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
 
     @Autowired
     private EmployeeService employeeService;
 
 
     @GetMapping("/getLibrarian")
-    public Employee getALibrarianInfo(int id) {
+    public Employee getALibrarianInfo(@RequestParam int id, HttpServletRequest httpServletRequest){
+        String id1 = httpServletRequest.getParameter("id");
+        System.out.printf("------>"+id1);
         //System.out.println("test :id: "+id);
         return employeeService.selectLibrarian(id);
+
     }
     @GetMapping(value = "/test")
     public String test(){
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.jasypt.intf.cli.JasyptPBEStringEncryptionCLI;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Value("${JASYPT_ENCRYPTOR_PASSWORD}")
+	private String passWord;//tJJT#qfZipW0seNf
+
 	@Test
 	public void contextLoads() {
+
+		String encryptString = "123456";
+		//需要加密的内容
+		String[] args = new String[3];
+		args[0] =  "input=" + encryptString;
+		args[1] = "password=" + passWord;
+		args[2] = "algorithm=PBEWithMD5AndDES";
+
+		JasyptPBEStringEncryptionCLI.main(args);
+
 	}
 
 }
